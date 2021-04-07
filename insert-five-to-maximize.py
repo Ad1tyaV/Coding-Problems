f2a16e39160ccf9fa0c90e065b6c13b2ca3c531d
Ad1tyaV <adithya.vadrevu@gmail.com>
2021-04-07 16:38:56 -0400

    Create insert-five-to-maximize.py

---
+++ insert-five-to-maximize.py
@@ -0,0 +1,23 @@
+inp = 9234
+target = 5
+m=None
+
+for i in range(0, len(str(inp))):
+	if target > int(str(inp)[i]):
+		m = i
+		break
+
+i = 0
+sum=0
+exp = len(str(inp))
+
+while exp>=0 :
+	if i==m:
+		sum+= (pow(10, exp) * target)
+		m=-1
+	else:
+		sum+= (pow(10, exp) * int(str(inp)[i]))
+		i+=1		
+	exp-=1
+
+print(sum)
