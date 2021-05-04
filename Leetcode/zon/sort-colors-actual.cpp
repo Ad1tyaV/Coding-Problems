177c99e7e771727ee2c906e35291e179873e219c
Ad1tyaV <adithya.vadrevu@gmail.com>
2021-05-04 00:15:24 -0400

    Create sort-colors-actual.cpp

---
+++ Leetcode/zon/sort-colors-actual.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+                
+       if(nums.size()==0 || nums.size()==1)
+           return;
+        
+        int start = 0;
+        int index = 0;
+        int end = nums.size()-1;
+        
+        while(index<=end && start < end){
+                        
+            if(nums[index]==0){
+                
+                nums[index] = nums[start];
+                nums[start] = 0;
+                index++;
+                start++;                    
+                
+            }
+            else if(nums[index]==2){
+                
+                nums[index] = nums[end];
+                nums[end] = 2;
+                end--;
+            }
+            else
+                index++;
+            
+        }
+        
+        
+        
+        
+    }
+};
