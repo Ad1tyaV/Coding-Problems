42b1be4491332cce27231e04eeec04212f64c6ff
Ad1tyaV <adithya.vadrevu@gmail.com>
2021-05-05 16:59:42 -0400

    Create copy-list-random-pointer.cpp

---
+++ Leetcode/zon/copy-list-random-pointer.cpp
@@ -0,0 +1,56 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+/*
+
+https://leetcode.com/problems/copy-list-with-random-pointer
+
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        
+        if(!head)
+            return head;
+        
+        map<Node*, pair<Node*, Node*>> mp;
+        map<Node*, Node*> oldNew;
+        
+        Node* h;
+        h = head;
+        
+        while(h){            
+            Node* temp;
+            temp = new Node(h->val);
+            oldNew[h] = temp;
+            mp[h] = {h->next, h->random};            
+            h=h->next;
+        }
+         
+        
+        for(map<Node*, pair<Node*, Node*>>::iterator it = mp.begin(); it!=mp.end(); it++){
+            
+            Node* temp;
+            temp = oldNew[(it->first)];
+            temp->next = oldNew[(it->second).first];
+            temp->random = oldNew[(it->second).second];
+            
+        }
+        
+        return oldNew[head];
+    }
+};
