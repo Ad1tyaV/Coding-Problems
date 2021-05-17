6e9ed7d515c49f37e25597086c71d63ffff6d7b0
Ad1tyaV <adithya.vadrevu@gmail.com>
2021-05-17 16:32:17 -0400

    Create delete-node-without-head.cpp

---
+++ Leetcode/LinkedLists/delete-node-without-head.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        
+        ListNode* temp;
+        temp = node;
+        node = node->next;
+        
+        while(node->next){
+            
+            temp->val = node->val;
+            temp = temp->next;
+            node = node->next;
+            
+        }
+        temp->val = node->val;
+        temp->next = NULL;
+        
+        
+        
+    }
+};
