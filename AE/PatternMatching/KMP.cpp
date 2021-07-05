1cc2eb30deabe0051643abfdb8421abf178787ee
Ad1tyaV <adithya.vadrevu@gmail.com>
2021-07-05 16:35:00 -0400

    Update KMP.cpp

--- AE/PatternMatching/KMP.cpp
+++ AE/PatternMatching/KMP.cpp
@@ -1,85 +1,106 @@
+#include<iostream>
 #include<vector>
 
 using namespace std;
 
 vector<int> buildPrefixTable(string matchPattern){
+    int updateIndex = 1;
+    int pivot = 0;
 
-	int updateIndex = 1;
-	int pivot = 0;
-	
-	vector<int> prefix_table(matchPattern.size(), -1);
-	
-	
-	prefix_table[0] = -1;
-	
-	while(updateIndex<matchPattern.size()){
-		
-		if(matchPattern[updateIndex]==matchPattern[pivot]){
-			prefix_table[updateIndex] = pivot;
-			pivot++;
-			updateIndex++;
-		}
-		else{
-			
-			if(pivot>0){
-				pivot = prefix_table[pivot-1] + 1;
-			}
-			else{
-				updateIndex++;
-			}
-			
-		}
-		
-	}
-					
-	return prefix_table;	
+    vector<int> prefix_table(matchPattern.size(), -1);
+
+    while(updateIndex<matchPattern.size()){
+
+        if(matchPattern[updateIndex]==matchPattern[pivot]){
+            prefix_table[updateIndex] = pivot;
+            updateIndex++;
+            pivot++;
+        }
+        else{
+
+            if(pivot>0){
+                pivot = prefix_table[pivot-1]+1;
+            }
+            else{
+                updateIndex++;
+            }
+
+
+        }
+    }
+    return prefix_table;
 }
 
+void print(vector<int> prefix_table){
 
-bool isMatched(string str, string substr, vector<int> prefix){
-	
-	int mainPointer = 0;
-	int subPointer = 0;
-	
-	while(mainPointer+subPointer<str.size()){
-		
-		int pointer1 = mainPointer;
-		int pointer2 = subPointer;
-		
-		bool matched = true;
-		while(pointer1<str.size() && pointer2<substr.size()){
-			if(str[pointer1]==substr[pointer2]){
-				pointer1++;
-				pointer2++;
-			}
-			else{
-				if(pointer2>0){					
-					pointer2 = prefix[pointer2-1] + 1;
-				}
-				else{
-					pointer1++;
-					matched = false;
-				}
-			}
-		}
-		
-		if(matched)
-			return true;
-		
-		mainPointer++;
-	}
-	
-	return false;
+    cout<<"Prefix Table:"<<endl;
+    for(int prefix:prefix_table)
+        cout<<prefix<<"\t";
+    
+    cout<<endl;
+
 }
 
-bool knuthMorrisPrattAlgorithm(string str, string substr) {
-  
-	vector<int> prefix_table = {};
-	prefix_table = buildPrefixTable(substr);
-	
-	
-	return isMatched(str, substr, prefix_table);	
-	
-	
-  return false;
+bool isSubpattern(string pattern, string subpattern, vector<int> prefix){
+
+    int mainPointer = 0;
+    int subPointer = 0;
+
+    while(mainPointer<pattern.size()){
+
+        int pointer = mainPointer;
+        int subPointer = subPointer;
+        bool matched = true;
+
+        while(pointer<pattern.size() && subPointer<subpattern.size()){
+
+            if(pattern[pointer]==subpattern[subPointer]){
+                pointer++;
+                subPointer++;
+            }
+            else{
+
+                if(subPointer>0){
+                    subPointer = prefix[subPointer-1] + 1;
+                }
+                else{
+                    pointer++;
+                    matched = false;                    
+                }
+
+            }
+        }
+
+        if(matched)
+            return matched;
+
+        mainPointer++;
+    }
+
+    return false;
+}
+
+int main(){
+
+    vector<string> mainStrings = {"aefoaefcdaefcdaed", "testwafwafawfawfawfawfawfawfawfa", "aaabaabacdedfaabaabaaa", "decadaafcdf"};
+    vector<string> matchPatterns = {"aefaedaefaefa", "fawfawfawfawfa", "aabaabaaa", "daf"};
+    
+    vector<int> prefix_table;    
+
+    for(int i=0; i<mainStrings.size(); i++){
+
+        string matchPattern = matchPatterns[i];
+        string mainString = mainStrings[i];
+
+        prefix_table = buildPrefixTable(matchPattern);
+                
+
+        if(isSubpattern(mainString, matchPattern, prefix_table))
+            cout<<"Yes "<<mainString<<" and "<<matchPattern<<" are matched"<<endl;
+        else
+            cout<<"No, "<<mainString<<" and "<<matchPattern<<" are not matched"<<endl;
+    
+    }
+
+    return 0;
 }
